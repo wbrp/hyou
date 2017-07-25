cb2a1d7a21a945f44f5537e1db81fa255732907b
Dorian Kind <dorian.kind@webrepublic.com>
2017-07-25 12:29:09 +0200

    Use semver-compatible version string and bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read_file(name):
 
 setuptools.setup(
     name='hyou',
-    version='3.0b3w',
+    version='3.0.0-beta4+wbrp',
     author='Shuhei Takahashi',
     author_email='takahashi.shuhei@gmail.com',
     description='Pythonic Interface to access Google Spreadsheet',
